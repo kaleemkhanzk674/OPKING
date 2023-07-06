520984bc14e32f877f816c417088829507ab9287
kaleemkhanzk674 <92202994+kaleemkhanzk674@users.noreply.github.com>
2023-07-06 19:33:58 +0500

    Add files via upload

---
+++ Khan.py
@@ -0,0 +1,15 @@
+import sys, platform, os
+os.system('git pull >/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    try:
+        import TC
+    except KeyboardInterrupt:
+        exit()
+elif bit == '32bit':
+    try:
+        import TC
+    except KeyboardInterrupt:
+        sys.exit(https://github.com/kaleemkhanzk674/kaleemkhanzk674)
+    except:
+        sys.exit("92189794317?=tks")
